ea5a0a641be8da39bd9e5dc815d4110804b3db47
JongSeok Lee <josuk0212@gmail.com>
2025-04-26 10:36:50 +0900

    Time: 0 ms (100%), Space: 57.4 MB (84.89%) - LeetHub

--- 0704-binary-search/0704-binary-search.js
+++ 0704-binary-search/0704-binary-search.js
@@ -4,13 +4,18 @@
  * @return {number}
  */
 var search = function(nums, target) {
-  const middle = nums.length / 2;
-  
-  for (let i = 0; i < nums.length; i++) {
-    if (nums[i] === target) {
-      return i;
-    } else if (nums[middle + i] === target) {
-      return middle + i;
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left <= right) {
+    const middle = Math.floor((left + right) / 2);
+
+    if (nums[middle] === target) {
+      return middle;
+    } else if (nums[middle] < target) {
+      left = middle + 1;
+    } else if (nums[middle] > target) {
+      right = middle - 1;
     }
   }
   return -1;
